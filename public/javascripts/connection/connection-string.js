b1430291e44f79a87cb52e16947fbfb15360950a
Fabio Pereira <famp@operamail.com>
2017-01-25 16:56:14 -0200

    ignore

--- public/javascripts/connection/connection-string.js
+++
@@ -1,5 +0,0 @@
-var exports = module.exports = {};
-
-exports.getConnectionString = function () {
-    return('mongodb://pennydreadfuladmin:knj52ev0ktyr@ds127439.mlab.com:27439/penny-dreadfuldb');
-}
