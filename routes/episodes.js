a4304b565277522a4f5af396d213d7efc571e65d
Fabio Pereira <famp@operamail.com>
2017-02-07 18:11:43 -0200

    Removida página que apresenta os pôsteres de cada season (/episode). Modificada URL das página de cada um dos episódios. Item Episodes da nav-bar agora é um dropdown. Modificada página com lista de episódios de uma temporada.

--- routes/episodes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET episodes page. */
-router.get('/', function(req, res) {
-    res.render('episodes');
-});
-
-module.exports = router;
--- routes/episodes/episode.js
+++ routes/episodes/episode.js
@@ -7,9 +7,22 @@ Episode = require('../../model/episode');
 
 /* GET a character's information page. */
 router.get('/', function(req, res) {
+    // Splits the request url which was divided by '/' to get the tokens
     var baseUrlArrayStrip = req.baseUrl.split("/");
+    // Getting the last character of the second token (seasonX) to be the season number
     var seasonNumber = baseUrlArrayStrip[2].charAt(baseUrlArrayStrip[2].length-1);
-    var episodeNumber = baseUrlArrayStrip[3].charAt(baseUrlArrayStrip[3].length-1);
+    var episodeNumber;
+    // Getting the last character of the third token (episodeX) if the episode number is not '10',
+    // otherwise get the last two characters
+    if(Number(baseUrlArrayStrip[3].charAt(baseUrlArrayStrip[3].length-1)) === 0
+        && Number(baseUrlArrayStrip[3].charAt(baseUrlArrayStrip[3].length-2)) === 1){
+        episodeNumber = Number(""
+            + baseUrlArrayStrip[3].charAt(baseUrlArrayStrip[3].length-2)
+            + baseUrlArrayStrip[3].charAt(baseUrlArrayStrip[3].length-1));
+    }
+    else {
+        episodeNumber = baseUrlArrayStrip[3].charAt(baseUrlArrayStrip[3].length-1);
+    }
 
     mongoose.connect(connectionString.getConnectionString());
 
